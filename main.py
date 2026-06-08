f8f2f1377c29b4541ed28f696bb51531609fab1a
moulesh <mouleshleo06@gmail.com>
2026-06-09 00:30:23 +0530

    phase 1 argparse work

---
+++ main.py
@@ -0,0 +1,37 @@
+from argparse import ArgumentParser
+
+def search(args):
+    print(args.book_name)
+    
+
+def recommend(args):
+    print(args.genre)
+
+def track(args):
+    print(args.book_name)
+
+
+def main():
+
+    parser = ArgumentParser(prog="shelfmate")
+
+    subparser = parser.add_subparsers(dest="command", help="commands like search, recommend, etc ...", required=True)
+
+    search_parser = subparser.add_parser("search", help="search for the books")
+    search_parser.add_argument("book_name", help="enter the book name")
+    search_parser.set_defaults(func=search)
+
+    recommend_parser = subparser.add_parser("recommend")
+    recommend_parser.add_argument("genre")
+    recommend_parser.set_defaults(func=recommend)
+
+    track_parser = subparser.add_parser("track")
+    track_parser.add_argument("book_name")
+    track_parser.set_defaults(func=track)
+
+
+    args = parser.parse_args()  
+    args.func(args)
+
+if __name__ == "__main__":
+    main()
